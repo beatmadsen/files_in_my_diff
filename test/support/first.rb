1d20bfa8fcf2af08b35bbaabd0c63849a1a33318
Erik T. Madsen <erik.thyge.madsen@wbd.com>
2024-11-01 14:44:18 +0100

    Separating out git setup into separate file

--- test/support/first.rb
+++ test/support/first.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def first
   puts 'first'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,8 @@ require 'files_in_my_diff'
 
 require 'minitest/autorun'
 
+require_relative 'git_helper'
+
 require 'active_support'
 require 'active_support/testing/parallelization'
 require 'active_support/testing/parallelize_executor'
@@ -15,34 +17,3 @@ Minitest.parallel_executor = ActiveSupport::Testing::ParallelizeExecutor.new(
   with: :processes,
   threshold: 0,
 )
-
-require 'git'
-require 'fileutils'
-require 'tmpdir'
-
-GIT_PROJECT_PATH = Dir.mktmpdir
-
-# Set up a dummy Git project before running tests
-def setup_dummy_git_project
-  repo = Git.init(GIT_PROJECT_PATH)
-
-  ['first.rb', 'second.rb', 'third.rb'].each do |file_name|
-    source_file = File.join(FilesInMyDiff.root, 'test', 'support', file_name)
-    dest_file = File.join(GIT_PROJECT_PATH, file_name)
-    FileUtils.cp(source_file, dest_file)
-    repo.add(file_name)
-    repo.commit("Add #{file_name}")
-  end
-end
-
-def teardown_dummy_git_project
-  if GIT_PROJECT_PATH && Dir.exist?(GIT_PROJECT_PATH)
-    FileUtils.rm_rf(GIT_PROJECT_PATH)
-  end
-end
-
-# Tear down the dummy Git project after all tests have run
-Minitest.after_run { teardown_dummy_git_project }
-
-# Run setup before all tests
-setup_dummy_git_project
