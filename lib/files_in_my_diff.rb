18c4f3aab9e7a565f094b4fdc67009e8aae0af12
Erik T. Madsen <erik.thyge.madsen@wbd.com>
2024-11-01 17:13:37 +0100

    green

--- lib/files_in_my_diff.rb
+++ lib/files_in_my_diff.rb
@@ -8,5 +8,31 @@ module FilesInMyDiff
   end
 
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.checkout(folder:, commit:)
+    Main.new(folder: folder, commit: commit).call
+  end
+
+  class Main
+    def initialize(folder:, commit:)
+      @folder = folder
+      @commit = commit
+    end
+
+    def call
+      validate_folder!
+      validate_commit!
+      { dummy: true }
+    end
+
+    private
+
+    def validate_folder!
+      raise ArgumentError, 'Folder is required' if @folder.nil?
+    end
+
+    def validate_commit!
+      raise ArgumentError, 'Commit is required' if @commit.nil?
+    end
+  end
 end
--- test/files_in_my_diff_test.rb
+++ test/files_in_my_diff_test.rb
@@ -6,4 +6,8 @@ class FilesInMyDiffTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::FilesInMyDiff::VERSION
   end
+
+  def test_that_main_validates_folder
+    assert_raises(ArgumentError) { FilesInMyDiff::Main.new(folder: nil, commit: 'HEAD').call }
+  end
 end
