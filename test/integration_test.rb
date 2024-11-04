889194cb8352211266548e1526415080178c3249
Erik T. Madsen <erik.thyge.madsen@wbd.com>
2024-11-04 16:32:32 +0100

    More thorough integration test

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -4,9 +4,16 @@ require 'test_helper'
 
 class IntegrationTest < Minitest::Test
   def test_that_main_interface_returns_non_empty_result_for_valid_input
-    changes = FilesInMyDiff.checkout(folder: GIT_PROJECT_PATH, revision: 'HEAD')
+    result = FilesInMyDiff.checkout(folder: GIT_PROJECT_PATH, revision: 'HEAD')
+    result => { dir:, sha:, changes: }
 
-    refute_nil changes
-    refute_empty changes
+    refute_nil dir
+    refute_nil sha
+    has_correct_changes = changes&.any? && changes.all? do |change|
+      fp = change[:full_path]
+      !fp.nil? && !fp.empty? && fp.start_with?(dir) && fp.include?(sha)
+    end
+
+    assert has_correct_changes, "Changes are not valid: #{changes.inspect}"
   end
 end
